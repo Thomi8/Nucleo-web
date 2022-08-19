c722c0909e9cb883f6a5d37a252d61a222858de9
Thomi8 <vicecontethomas@gmail.com>
2022-08-19 13:51:33 -0300

    corrigiendo primer desafio

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,12 @@
 const NavBar = () => {
     return (
         <div>
-            <h1 class="main-header__title">NÚCLEO</h1>
-                <nav class="main-nav" id="main-nav">
-                    <ul class="menu">
-                        <li class="menu__item"><a href="index.html" class="menu__link">HOME</a></li>
-                        <li class="menu__item"><a href="./html/mercado.html" class="menu__link">MERCADO</a></li>
-                        <li class="menu__item"><a href="./html/contacto.html" class="menu__link">CONTACTO</a></li>
+            <h1 className="main-header__title">NÚCLEO</h1>
+                <nav className="main-nav" id="main-nav">
+                    <ul className="menu">
+                        <li className="menu__item"><a href="index.html" className="menu__link">HOME</a></li>
+                        <li className="menu__item"><a href="./html/mercado.html" className="menu__link">MERCADO</a></li>
+                        <li className="menu__item"><a href="./html/contacto.html" className="menu__link">CONTACTO</a></li>
                     </ul>
                 </nav>
         </div>
