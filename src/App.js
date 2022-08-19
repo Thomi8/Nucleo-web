929e057933c17fa2ebc3817d4bacee47cee0359f
Thomi8 <vicecontethomas@gmail.com>
2022-08-19 13:06:37 -0300

    agregando navbar

--- src/App.js
+++ src/App.js
@@ -3,22 +3,30 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="main-header">
+        <div  className="container container--flex">
+            <div  className="main-header__container">
+                <h1  className="main-header__title">NÚCLEO</h1>
+                <span  className="icon-menu" id="btn-menu"><i  className="fas fa-bars"></i></span>
+                <nav  className="main-nav" id="main-nav">
+                    <ul  className="menu">
+                        <li  className="menu__item"><a href="index.html" className="menu__link">HOME</a></li>
+                        <li  className="menu__item"><a href="./html/mercado.html"  className="menu__link">MERCADO</a></li>
+                        <li  className="menu__item"><a href="./html/contacto.html"  className="menu__link">CONTACTO</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div  className="main-header__container">
+                <span  className="main-header__txt">Necesitas ayuda?</span>
+                <p  className="main-header__txt"><i  className="fas fa-phone"></i> Comunicate al (+54 9)223-544-74-57</p>
+            </div>
+            <div  className="main-header__container">
+                <a href=""  className="main-header__link"><i  className="fas fa-user"></i></a>
+                <a href=""  className="main-header__btn">Mi carrito <i  className="fas fa-shopping-cart"></i></a>
+                <input  type="search"  className="main-header__input" placeholder="Buscar productos"></input><i  className="fas fa-search"></i>
+            </div>
+        </div>
+    </header>
   );
 }
 
