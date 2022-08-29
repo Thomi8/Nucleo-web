9ff16d21fd04c8b8f6d50055a30632b8f73b0c98
Thomi8 <vicecontethomas@gmail.com>
2022-08-29 15:21:03 -0300

    promesa y map

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -8,7 +8,7 @@ export default function ItemList(props) {
     return(
         <div className="bg-white">
         <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 lg:pt-12">
-            <h2 id="products" className="text-2xl font-bold tracking-tight text-gray-900">Productos destacados</h2>
+            <h2 id="products" className="text-2xl font-bold tracking-tight text-gray-900">Lo nuevo que tenemos!</h2>
             <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {items.map((item) => (
                 <Item
